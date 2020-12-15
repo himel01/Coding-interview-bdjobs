38d9eb0c98513473af70d6496bfd8185cd019aea
Aqemur Reza Himel <aqemur.reza@gmail.com>
2020-12-15 14:34:07 +0600

    created apiclient class and added retrofit dependencies

---
+++ app/src/main/java/com/example/jobhuntjava/ApiClient.java
@@ -0,0 +1,6 @@
+package com.example.jobhuntjava;
+
+public class ApiClient {
+    private static String BASE_URL="http://corporate3.bdjobs.com/interviewtest/InterviewJson.json";
+    private static ApiClient mInstance;
+}
