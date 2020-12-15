2e7716d9ca6206470257620ee5d3748b41964426
Aqemur Reza Himel <aqemur.reza@gmail.com>
2020-12-15 17:31:18 +0600

    created adapter and edited main activity

---
+++ app/src/main/java/com/example/jobhuntjava/JobAdapter.java
@@ -0,0 +1,62 @@
+package com.example.jobhuntjava;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class JobAdapter extends RecyclerView.Adapter<JobAdapter.ViewHolder> {
+    private ArrayList<JobInfo> jobInfos;
+    private Context context;
+
+    public JobAdapter(ArrayList<JobInfo> jobInfos, Context context) {
+        this.jobInfos = jobInfos;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.model_job,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+       JobInfo jobInfo= jobInfos.get(position);
+       holder.jobTitle.setText(jobInfo.getJobTitle());
+       holder.companyName.setText(jobInfo.getJobDetails().getCompanyName());
+       holder.deadline.setText(jobInfo.getDeadline());
+       holder.experience.setText(jobInfo.getMinExperience());
+       holder.companyLogo.setImageResource(Integer.parseInt(jobInfo.getLogo()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return jobInfos.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView jobTitle,companyName,deadline,experience;
+        private ImageView companyLogo;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            jobTitle=itemView.findViewById(R.id.jobTitleTV);
+            companyName=itemView.findViewById(R.id.companyNameTV);
+            deadline=itemView.findViewById(R.id.deadLineTV);
+            experience=itemView.findViewById(R.id.experienceTV);
+            companyLogo=itemView.findViewById(R.id.companyIV);
+        }
+    }
+}
--- app/src/main/java/com/example/jobhuntjava/MainActivity.java
+++ app/src/main/java/com/example/jobhuntjava/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.jobhuntjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private JobAdapter adapter;
+    private ArrayList<JobInfo> infoArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        recyclerView=findViewById(R.id.jobRV);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        infoArrayList=new ArrayList<>();
     }
 }
