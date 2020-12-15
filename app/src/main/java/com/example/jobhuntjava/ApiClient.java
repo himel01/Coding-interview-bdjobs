314ebdd4be351b7bb0ce2ffc9358c6528fdb963d
Aqemur Reza Himel <aqemur.reza@gmail.com>
2020-12-15 14:48:37 +0600

    created retrofitInterface and edited apiclient

--- app/src/main/java/com/example/jobhuntjava/ApiClient.java
+++ app/src/main/java/com/example/jobhuntjava/ApiClient.java
@@ -1,6 +1,31 @@
 package com.example.jobhuntjava;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiClient {
     private static String BASE_URL="http://corporate3.bdjobs.com/interviewtest/InterviewJson.json";
     private static ApiClient mInstance;
+    private Retrofit retrofit;
+
+
+    public ApiClient()
+    {
+        retrofit=new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+    public ApiClient getInstance()
+    {
+        if(mInstance==null)
+        {
+            mInstance=new ApiClient();
+        }
+        return mInstance;
+    }
+    public RetrofitInterface getApi()
+    {
+        return retrofit.create(RetrofitInterface.class);
+    }
 }
---
+++ app/src/main/java/com/example/jobhuntjava/RetrofitInterface.java
@@ -0,0 +1,4 @@
+package com.example.jobhuntjava;
+
+public interface RetrofitInterface {
+}
