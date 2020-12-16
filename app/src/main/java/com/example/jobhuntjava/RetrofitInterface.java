c35cea5ce21c5cf6f61c11e4c1014788d2b3b801
Aqemur Reza Himel <aqemur.reza@gmail.com>
2020-12-16 23:39:10 +0600

    minor change

--- app/src/main/java/com/example/jobhuntjava/RetrofitInterface.java
+++ app/src/main/java/com/example/jobhuntjava/RetrofitInterface.java
@@ -6,7 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface RetrofitInterface {
-    @GET("posts")
+    @GET("InterviewJson.json")
     Call<ArrayList<JobInfo>> getData();
 
 }
